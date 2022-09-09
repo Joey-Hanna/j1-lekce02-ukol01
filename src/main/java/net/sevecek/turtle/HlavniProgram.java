baa0f8d849a44a12897599fc20d45daa21d21eb9
Joey-Hanna <joey.martin.de.lange@gmail.com>
2022-09-09 15:15:39 +0200

    nakresli prasatko

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,48 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+
+        nakresliPrasatko(zofka);
+
+
     }
 
+    private void nakresliPrasatko(Turtle zofka) {
+        nakresliDomecekVlevo(zofka);
+        nakresliNozicky(zofka);
+        zofka.turnRight(135);
+        zofka.move(100);
+        zofka.turnRight(135);
+        zofka.move(25);
+        nakresliDalsiNozicky(zofka);
+    }
+
+    private void nakresliDomecekVlevo(Turtle zofka) {
+        for (int i = 0; i < 5; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+        zofka.turnLeft(225);
+        zofka.move(70);
+        for (int i = 0; i < 2; i++) {
+            zofka.turnLeft(45);
+        }
+        zofka.move(70);
+    }
+
+    private void nakresliDalsiNozicky(Turtle zofka) {
+        zofka.turnLeft(180);
+        zofka.move(25);
+        zofka.turnRight(90);
+        zofka.move(25);
+    }
+
+    private void nakresliNozicky(Turtle zofka) {
+        for (int i = 0; i < 2; i++) {
+            zofka.turnRight(90);
+            zofka.move(25);
+            zofka.turnLeft(180);
+            zofka.move(25);
+        }
+    }
 }
